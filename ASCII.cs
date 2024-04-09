2d88de354b9c75f7181dbfdffbd5ba4563f2b291
diexban <166546790+diexban@users.noreply.github.com>
2024-04-09 21:14:41 +0200

    Create ASCII.cs

---
+++ ASCII.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Code Prints Out ASCII art
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");  
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Manfred The Ghost");            
+     }
+  }
+}
